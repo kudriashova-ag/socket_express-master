76386ccebcfaf27f048b74b26f176b1684bf6d32
Anastacia <kudriashova.ag@gmail.com>
2023-07-27 18:32:44 +0300

    cors2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-import cors from "cors";
+// import cors from "cors";
 import fs from "fs";
 // Validations
 import { authorizationValidator } from "./Validations/Authorization.js";
